bb87fa40fa0fb02f02da38abb3f8987595102cbc
aperepeliak <aperepelyak@gmail.com>
2016-10-07 15:18:52 +0300

    add save-load xml

--- LINQ/LearningLINQ/XMLmanip/Program.cs
+++ LINQ/LearningLINQ/XMLmanip/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Xml.Linq;
+
 namespace XMLmanip
 {
     class Program
     {
         static void Main(string[] args)
         {
+            XElement xcust =
+                new XElement("customers",
+                    new XElement("customer",
+                        new XAttribute("ID", "A"),
+                        new XAttribute("City", "New York"),
+                        new XAttribute("Region", "North America"),
+                        new XElement("order",
+                            new XAttribute("Item", "Widget"),
+                            new XAttribute("Price", 100)
+                            ),
+                        new XElement("order",
+                            new XAttribute("Item", "Tire"),
+                            new XAttribute("Price", 200)
+                            )
+                            ),
+                    new XElement("customer",
+                        new XAttribute("ID", "B"),
+                        new XAttribute("City", "Mumbai"),
+                        new XAttribute("Region", "Asia"),
+                        new XElement("order",
+                            new XAttribute("Item", "Oven"),
+                            new XAttribute("Price", 300)
+                        )
+                     )
+                 );
+
+            string xmlFileName = @"D:\Study\NETStudyRepo\LINQ\LearningLINQ\XMLmanip\fragment.xml";
+
+            xcust.Save(xmlFileName);
+
+            XElement xcust2 = XElement.Load(xmlFileName);
+
+            Console.WriteLine("Contents of xcust: ");
+            Console.Write(xcust);
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
