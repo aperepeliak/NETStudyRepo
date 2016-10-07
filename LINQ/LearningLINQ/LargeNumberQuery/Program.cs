8b5bd7c38ceeb66a2494aec02a50be6c1896b9fe
aperepeliak <aperepelyak@gmail.com>
2016-10-07 18:18:53 +0300

    add large number query

--- LINQ/LearningLINQ/LargeNumberQuery/Program.cs
+++ LINQ/LearningLINQ/LargeNumberQuery/Program.cs
@@ -10,6 +10,33 @@ namespace LargeNumberQuery
     {
         static void Main(string[] args)
         {
+            int[] numbers = GenerateLotsOfNumbers(12045678);
+
+            var queryResult = from n in numbers
+                              where n < 1000
+                              select n;
+
+            Console.WriteLine("Numbers less than 1000 : ");
+
+            foreach (var n in queryResult)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int[] GenerateLotsOfNumbers(int count)
+        {
+            Random generator = new Random(0);
+
+            int[] result = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = generator.Next();
+            }
+            return result;
         }
     }
 }
