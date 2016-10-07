a0e946f4044a58b4ba01ad692f201c44807d9adb
aperepeliak <aperepelyak@gmail.com>
2016-10-07 15:29:13 +0300

    added linq query

--- LINQ/LearningLINQ/LinqQuerySyntax/Program.cs
+++ LINQ/LearningLINQ/LinqQuerySyntax/Program.cs
@@ -10,6 +10,21 @@ namespace LinqQuerySyntax
     {
         static void Main(string[] args)
         {
+            string[] names = {"Pavel", "Pyotr", "Nazar", "Andrii", "Dimitrii", "Akis", "Rustam", "John",
+            "Akhmat", "Volodya", "Artem", "Vasilii"};
+
+            var queryResults = from n in names
+                               where n.StartsWith("A")
+                               select n;
+
+            Console.WriteLine("Names beginning with A : ");
+            foreach (var item in queryResults)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Delay
+            Console.ReadKey();
         }
     }
 }
