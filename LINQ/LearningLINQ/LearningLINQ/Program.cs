a0542ee1b12ff686eba8ff062860d7c59e494dc4
aperepeliak <aperepelyak@gmail.com>
2016-10-07 12:08:27 +0300

    created linq to xml constructor

---
+++ LINQ/LearningLINQ/LearningLINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+
+namespace LearningLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xdoc = new XDocument(
+                new XElement("customers",
+                    new XElement("customer",
+                        new XAttribute("ID", "A"),
+                        new XAttribute("City", "New York"),
+                        new XAttribute("Region", "North America"),
+                        new XElement("order",
+                            new XAttribute("Item", "Widget"),
+                            new XAttribute("Price", 100)
+                            ),
+                        new XElement("order",
+                            new XAttribute("Item", "Tire"),
+                            new XAttribute("Price", 200)
+                            )
+                            ),
+                    new XElement("customer",
+                        new XAttribute("ID", "B"),
+                        new XAttribute("City", "Mumbai"),
+                        new XAttribute("Region", "Asia"),
+                        new XElement("order",
+                            new XAttribute("Item", "Oven"),
+                            new XAttribute("Price", 300)
+                        )
+                     )
+                 )
+             );
+            Console.WriteLine(xdoc);
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
