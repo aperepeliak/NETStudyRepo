d88c9b360d0c5b371977bd53eaa58b522ef13cc6
aperepeliak <aperepelyak@gmail.com>
2016-10-13 19:28:15 +0300

    codeFirst approach but without dbInitializer check

---
+++ EntityFramework/CodeFirstDb/CodeFirstDb/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDb
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Key]
+        public int Code { get; set; }
+    }
+}
---
+++ EntityFramework/CodeFirstDb/CodeFirstDb/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CodeFirstDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BookContext())
+            {
+                Book book1 = new Book { Title = "Beginning C#", Author = "Perkins" };
+                db.Books.Add(book1);
+
+                Book book2 = new Book { Title = "LearnJS", Author = "Kantor" };
+                db.Books.Add(book2);
+
+                db.SaveChanges();
+
+                var query = from b in db.Books
+                            orderby b.Title
+                            select b;
+
+                Console.WriteLine("All books in db : ");
+
+                foreach (var b in query)
+                {
+                    Console.WriteLine($"{b.Title} by {b.Author}, code = {b.Code}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
