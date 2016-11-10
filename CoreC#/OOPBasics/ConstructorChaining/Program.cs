d774be40985d2fe0eaf26b89b6fc94533d2e187f
aperepeliak <aperepelyak@gmail.com>
2016-11-10 17:42:12 +0200

    learning OOP basics

---
+++ CoreC#/OOPBasics/ConstructorChaining/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Motorcycle
+    {
+        public int driverIntensity;
+        public string driverName;
+
+        // Constructor chaining
+        public Motorcycle(int intensity)
+            : this(intensity, "") { }
+
+        public Motorcycle(string name)
+            : this(0, name) { }
+
+        // Master constructor
+        public Motorcycle(int intensity, string name)
+        {
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverIntensity = intensity;
+            driverName = name;
+        }
+    }
+}
