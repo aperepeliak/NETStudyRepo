ebb68d6b20aea895c4f4e345fc33f8fb60d55703
aperepeliak <aperepelyak@gmail.com>
2016-11-10 21:21:09 +0200

    learning static types

---
+++ CoreC#/OOPBasics/StaticData/StaticClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticData
+{
+    static class StaticClass
+    {
+        public static void PrintTime() { Console.WriteLine(DateTime.Now.ToShortTimeString()); }
+        public static void PrintDate() { Console.WriteLine(DateTime.Now.ToShortDateString()); }
+    }
+}
