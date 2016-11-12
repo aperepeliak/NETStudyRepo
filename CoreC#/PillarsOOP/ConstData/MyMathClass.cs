f3d97199c2b98b421d17dfcab0be98cd992b6787
aperepeliak <aperepelyak@gmail.com>
2016-11-12 20:57:03 +0200

    learning const, static and readonly keywords

---
+++ CoreC#/PillarsOOP/ConstData/MyMathClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        // Static implicitly
+        public const double PI = 3.14;
+
+        // We can assign in ctor but nowhere else
+        public readonly double PI2;
+
+        public static readonly double PI3;
+
+        public MyMathClass()
+        {
+            PI2 = 3.14;
+        }
+
+        // Static ctor for static members
+        static MyMathClass()
+        {
+            PI3 = 3.14;
+        }
+    }
+}
---
+++ CoreC#/PillarsOOP/ConstData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning const data");
+            Console.WriteLine($"The value of PI: {MyMathClass.PI}");
+        }
+    }
+}
