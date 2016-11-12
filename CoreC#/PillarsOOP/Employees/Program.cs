739e4c6467921ff4549d190abefd2d8a51e73eb4
aperepeliak <aperepelyak@gmail.com>
2016-11-12 21:37:19 +0200

    learning inheritance is-a

---
+++ CoreC#/PillarsOOP/Employees/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee class hierarchy");
+
+            SalesPerson fred = new SalesPerson();
+            fred.Age = 34;
+            fred.Name = "Fred";
+            fred.SalesNumber = 50;
+        }
+    }
+}
