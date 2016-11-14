c48ad8aa898068f1a3f14c3b317c340bb9157260
aperepeliak <aperepelyak@gmail.com>
2016-11-14 21:58:49 +0200

    learning exception handling

---
+++ CoreC#/PillarsOOP/SimpleException/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Exception example");
+
+            Console.WriteLine(" => creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    myCar.Accelerate(10);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n Error!");
+                Console.WriteLine("Member name: {0}", e.TargetSite);
+
+                Console.WriteLine("Class defining member: {0}", e.TargetSite.DeclaringType);
+                Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);
+
+                Console.WriteLine("Message: {0}", e.Message);
+                Console.WriteLine("Source: {0}", e.Source);
+            }
+            
+        }
+    }
+}
