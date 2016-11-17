d71cb0d9cb9ddb730232615b32744d296a6e3f37
aperepeliak <aperepelyak@gmail.com>
2016-11-17 21:52:11 +0200

    learning name clashes among interfaces and ones explicit implementation

--- CoreC#/PillarsOOP/CustomInterface/Program.cs
+++ CoreC#/PillarsOOP/CustomInterface/Program.cs
@@ -21,12 +21,26 @@ namespace CustomInterface
                 if (shape is IDraw3D)
                     DrawIn3D((IDraw3D)shape);
             }
+
+            IPointy firstPointyItem = FindFirstPointyShape(myShapes);
+            if (firstPointyItem != null)
+                Console.WriteLine($"\n\nThe first Pointy item is: {firstPointyItem} and has {firstPointyItem.Points} points");
         }
 
         static void DrawIn3D(IDraw3D drawMe)
         {
             Console.WriteLine(" -> Drawing IDraw3D compatible type");
             drawMe.Draw3D();
+        }
+
+        static IPointy FindFirstPointyShape(Shape[] shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                if (shape is IPointy)
+                    return shape as IPointy;
+            }
+            return null;
         }
     }
 }
---
+++ CoreC#/PillarsOOP/InterfaceNameClash/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Octagon oct = new Octagon();
+
+            // Draw() is not available through the dot operator because interfaces were implenented explicitly in the class
+            // to access the required functionality must use explicit casting
+
+            IDrawToForm toForm = (IDrawToForm)oct;
+            toForm.Draw();
+
+            // Shorthand notation if don't need to use the interface var later
+            ((IDrawToMemory)oct).Draw();
+
+            // Another option is to use the 'is' keyword
+            if (oct is IDrawToPrinter)
+                ((IDrawToPrinter)oct).Draw();
+        }
+    }
+
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        //public void Draw()
+        //{
+        //    Console.WriteLine("Shares draw method...");
+        //}
+
+        // No access modifiers!!
+        // Explicitly implemented interfaces are automatically private.
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to form...");
+        }
+
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to printer...");
+        }
+
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to memory...");
+        }
+    }
+
+    // Draw image to a form
+    public interface IDrawToForm
+    {
+        void Draw();
+    }
+
+    // Draw to buffer in memory
+    public interface IDrawToMemory
+    {
+        void Draw();
+    }
+
+    // Render to the printer
+    public interface IDrawToPrinter
+    {
+        void Draw();
+    }
+}
