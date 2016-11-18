a5d801166fbdccbeee1d4714c89d17e789f65bee
aperepeliak <aperepelyak@gmail.com>
2016-11-18 17:58:13 +0200

    learning interface hierarchies and multiple inheritance

---
+++ CoreC#/PillarsOOP/InterfaceHierarchy/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitmapImage myBitmap = new BitmapImage();
+
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(1,2,3,4);
+            myBitmap.DrawUpsideDown();
+
+            // Get IAdvancedDraw explicitly
+            IAdvancedDraw adv = myBitmap as IAdvancedDraw;
+            if (adv != null)
+                adv.DrawUpsideDown();
+        }
+    }
+
+    public interface IDrawable
+    {
+        void Draw();
+    }
+
+    public interface IAdvancedDraw : IDrawable
+    {
+        void DrawInBoundingBox(int top, int left, int bottom, int right);
+        void DrawUpsideDown();
+    }
+
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int bottom, int right)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down...");
+        }
+    }
+}
---
+++ CoreC#/PillarsOOP/MultipleInheritance/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    interface IDrawable
+    {
+        void Draw();
+    }
+
+    interface IPrintable
+    {
+        void Print();
+        void Draw(); // <-- Possible name clash
+    }
+
+    interface IShape : IDrawable, IPrintable
+    {
+        int GetNumberOfSides();
+    }
+
+    // Simple implicit implementation of the interface
+    class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing ...");
+        }
+
+        public int GetNumberOfSides()
+        {
+            return 4;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing ...");
+        }
+    }
+
+    class Square : IShape
+    {
+        void IPrintable.Draw()
+        {
+            Console.WriteLine("Drawing to printer ...");
+        }
+
+        void IDrawable.Draw()
+        {
+            Console.WriteLine("Drawing to screen ...");
+        }
+
+        int IShape.GetNumberOfSides()
+        {
+            return 4;
+        }
+
+        void IPrintable.Print()
+        {
+            Console.WriteLine("Printing ...");
+        }
+    }
+}
