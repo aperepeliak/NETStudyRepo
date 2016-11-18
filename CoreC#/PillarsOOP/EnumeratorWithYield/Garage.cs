e56fb2c89db0f91ff3f86fa8dfe4434f0ec00766
aperepeliak <aperepelyak@gmail.com>
2016-11-18 20:49:31 +0200

    learning yield return expression

---
+++ CoreC#/PillarsOOP/EnumeratorWithYield/Garage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorWithYield
+{
+    public class Garage : IEnumerable
+    {
+        private Car[] carArray = new Car[4];
+
+        public Garage()
+        {
+            carArray[0] = new Car("Rusty", 50);
+            carArray[1] = new Car("Clunker", 55);
+            carArray[2] = new Car("Daisy", 23);
+            carArray[3] = new Car("Philly", 45);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Car c in carArray)
+            {
+                yield return c;
+            }
+        }
+
+        public IEnumerable GetTheCars(bool returnReversed)
+        {
+            // Return the item in reverse
+            if (returnReversed)
+            {
+                for (int i = carArray.Length; i != 0; i--)
+                {
+                    yield return carArray[i - 1];
+                }
+            } else
+            {
+                // Return the items as placed in the array
+                foreach(Car c in carArray)
+                {
+                    yield return c;
+                }
+            }
+        }
+    }
+}
---
+++ CoreC#/PillarsOOP/EnumeratorWithYield/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratorWithYield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Garage carLot = new Garage();
+
+            // Get items using GetEnumerator()
+            foreach(Car c in carLot)
+            {
+                Console.WriteLine($"{c.PetName} is going {c.CurrSpeed} MPH");
+            }
+
+            Console.WriteLine();
+
+            // Get items (in reverse) using named iterator.
+            foreach (Car c in carLot.GetTheCars(true))
+            {
+                Console.WriteLine($"{c.PetName} is going {c.CurrSpeed} MPH");
+            }
+        }
+    }
+}
