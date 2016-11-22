02fe23a91dc2ef799ecf078c2b51912fb86485cc
aperepeliak <aperepelyak@gmail.com>
2016-11-22 18:00:22 +0200

    learning custom generic methods

---
+++ CoreC#/GenericCollections/003 - CustomGenericMthod/MyGenericMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003___CustomGenericMthod
+{
+    public static class MyGenericMethods
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine($"You sent the Swap() method a {typeof(T)}");
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void DisplayBaseClass<T>()
+        {
+            Console.WriteLine($"Base class of {typeof(T)} is {typeof(T).BaseType}");
+        }
+    }
+}
---
+++ CoreC#/GenericCollections/003 - CustomGenericMthod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003___CustomGenericMthod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Custom generic methods\n\n");
+
+            int a = 5, b = 99;
+
+            Console.WriteLine($"Before swap: {a}\t{b}");
+            MyGenericMethods.Swap<int>(ref a, ref b);
+            Console.WriteLine($"After swap: {a}\t{b}");
+
+            string s1 = "First", s2 = "Second";
+            Console.WriteLine($"\nBefore swap: {s1}\t{s2}");
+            MyGenericMethods.Swap<string>(ref s1, ref s2);
+            Console.WriteLine($"After swap: {s1}\t{s2}");
+
+            Console.WriteLine(new string('-', 20));
+            MyGenericMethods.DisplayBaseClass<int>();
+            MyGenericMethods.DisplayBaseClass<string>();
+            MyGenericMethods.DisplayBaseClass<Type>();
+            MyGenericMethods.DisplayBaseClass<ValueType>();
+        }
+
+       
+    }
+}
