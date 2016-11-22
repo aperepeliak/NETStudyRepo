45e83412eae838c9a0d3a168f3a980cdf5bebcde
aperepeliak <aperepelyak@gmail.com>
2016-11-22 09:49:25 +0200

    learning IComparable standard interface

---
+++ CoreC#/PillarsOOP/ComparableCar/Car.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Car : IComparable
+    {
+        public const int MaxSpeed = 100;
+
+        public int CarID { get; set; }
+        public int CurrSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Property to return PetNameComparer
+        public static IComparer SortByPetName
+        {
+            get
+            {
+                return new PetNameComparer();
+            }
+        }
+
+        // Is the car still operational
+        private bool carIsDead;
+
+        private Radio theMusicBox = new Radio();
+
+        // Constructors
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrSpeed = speed;
+            PetName = name;
+        }
+        public Car(string name, int speed, int id) : this(name, speed)
+        {
+            CarID = id;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object
+            theMusicBox.TurnOn(state);
+        }
+
+        // See if car has overheated
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Console.WriteLine($"{PetName} is out of order.");
+            }
+            else
+            {
+                CurrSpeed += delta;
+                if (CurrSpeed > MaxSpeed)
+                {
+                    Console.WriteLine($"{PetName} has overheated.");
+                    CurrSpeed = 0;
+                    carIsDead = true;
+                    Exception ex = new Exception(string.Format("{0} has overheated!", PetName));
+                    ex.HelpLink = "https://google.com";
+
+                    ex.Data.Add("Timestamp", string.Format("The car exploded at: {0}", DateTime.Now));
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
+
+                }
+                else
+                {
+                    Console.WriteLine($" => Current speed = {CurrSpeed}");
+                }
+            }
+        }
+
+        public int CompareTo(object obj)
+        {
+            Car temp = obj as Car;
+
+            if (temp != null)
+            {
+                if (this.CarID > temp.CarID)
+                {
+                    return 1;
+                }
+                if (this.CarID < temp.CarID)
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Sorry, invalid argument.");
+            }
+        }
+
+        // Possible simplier implementation
+        //public int CompareTo(object o)
+        //{
+        //    Car c = o as Car;
+        //    if (c != null)
+        //        return this.CarID.CompareTo(c.CarID);
+        //    else
+        //        throw new ArgumentException("Sorry.");
+        //}
+    }
+}
---
+++ CoreC#/PillarsOOP/ComparableCar/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparableCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] myAutos = new Car[] {
+                new Car("Daisy", 60, 12),
+                new Car("Philly", 50, 5),
+                new Car("Mary", 55, 10),
+                new Car("Mike", 43, 23)
+            };
+
+            Console.WriteLine("Before sort: ");
+            foreach (Car car in myAutos)
+            {
+                Console.WriteLine($"{car.CarID}\t{car.PetName}");
+            }
+
+            Array.Sort(myAutos);
+
+            Console.WriteLine($"\nAfter sort #1: ");
+            foreach (Car car in myAutos)
+            {
+                Console.WriteLine($"{car.CarID}\t{car.PetName}");
+            }
+
+            //Array.Sort(myAutos, new PetNameComparer());
+
+            // Sorting is now more clear and understandable
+            Array.Sort(myAutos, Car.SortByPetName);
+            Console.WriteLine($"\nAfter sort #2: ");
+            foreach (Car car in myAutos)
+            {
+                Console.WriteLine($"{car.CarID}\t{car.PetName}");
+            }
+        }
+    }
+}
