ec51fb80f7a60b66deac018ad817aae3c21bde7b
aperepeliak <aperepelyak@gmail.com>
2016-11-23 14:03:04 +0200

    sending object state notifications using delegates

---
+++ CoreC#/LearningDelegates/002 - CarDelegate/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002___CarDelegate
+{
+    public class Car
+    {
+        // Internal state data
+        public int CurrrentSpeed { get; set; }
+        public int MaxSpeed { get; set; } = 100;
+        public string PetName { get; set; }
+
+        // Is the car alive or dead?
+        private bool carIsDead;
+
+        // Ctors
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // 1) Define a delegate type
+        public delegate void CarEngineHandler(string msgForCaller);
+
+        // 2) Define a member variable for this delegate
+        private CarEngineHandler listOfHandlers;
+
+        // 3) Add registration function for the caller
+        // += --> multicasting support
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers += methodToCall;
+        }
+
+        // "Unsubscribe" from a given notification at runtime
+        public void UnregisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listOfHandlers -= methodToCall;
+        }
+
+        // 4) Implement the Accelerate() method to invoke the delegate's
+        //    invocation list under the correct circumstances
+        public void Accelerate(int delta)
+        {
+            // If this car is "dead" send dead message
+            if (carIsDead)
+            {
+                if (listOfHandlers != null)
+                    listOfHandlers("Sorry, this car is dead...");
+            } else
+            {
+                CurrrentSpeed += delta;
+
+                // Is this car "almost dead"?
+                if (10 == (MaxSpeed - CurrrentSpeed) && listOfHandlers != null)
+                {
+                    listOfHandlers("Careful buddy! Gonna blow!");
+                }
+                if (CurrrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                } else
+                {
+                    Console.WriteLine($"Current speed = {CurrrentSpeed}");
+                }
+            }
+        }
+    }
+}
---
+++ CoreC#/LearningDelegates/002 - CarDelegate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002___CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegates as event enablers\n");
+
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Tell the car which method to call when it wants to send us messages
+            c1.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+
+            Car.CarEngineHandler handler2 = new Car.CarEngineHandler(OnCarEngineEventUpper);
+            c1.RegisterWithCarEngine(handler2);
+
+            // Speed up (this will trigger the events)
+            Console.WriteLine("Speeding up");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            // Unregister from handler2
+            c1.UnregisterWithCarEngine(handler2);
+            Console.WriteLine("Speeding up");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+        }
+
+        private static void OnCarEngineEventUpper(string msg)
+        {
+            Console.WriteLine($"=> {msg.ToUpper()}");
+        }
+
+        public static void OnCarEngineEvent(string msg)
+        {
+            Console.WriteLine("\n**** Message from car Object ****\n");
+            Console.WriteLine($"=> {msg}");
+            Console.WriteLine("************************\n");
+        }
+    }
+}
