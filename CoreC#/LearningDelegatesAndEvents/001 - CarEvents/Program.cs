c567c42de0272216fa43875e5bac4300d5804220
aperepeliak <aperepelyak@gmail.com>
2016-11-24 18:34:23 +0200

    learning events definition and usage

---
+++ CoreC#/LearningDelegatesAndEvents/001 - CarEvents/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001___CarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning events");
+
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Regestering event handlers
+            // c1.AboutToBlow += new Car.CarEngineHandler(CarIsAlmostDoomed);
+
+            // Using method group conversion
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+
+            //Car.CarEngineHandler d = new Car.CarEngineHandler(CarExploded);
+            //c1.Exploded += d;
+
+            c1.Exploded += CarExploded;
+
+            Console.WriteLine("**** Speeding up ****");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+
+            // Removing CarExploded method from invocation list
+            // c1.Exploded -= d;
+
+            c1.Exploded -= CarExploded;
+
+            Console.WriteLine("\n**** Speeding UP! ****\n");
+            for (int i = 0; i < 6; i++)
+            {
+                c1.Accelerate(20);
+            }
+        }
+
+        private static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine($" => Critical Message from Car: {msg}");
+        }
+    }
+}
