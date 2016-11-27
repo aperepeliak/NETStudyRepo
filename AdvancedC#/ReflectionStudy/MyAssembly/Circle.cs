e1752d2b6db4b630fd6c06a6fe0b757518cf6bb4
aperepeliak <aperepelyak@gmail.com>
2016-11-27 13:53:46 +0200

    project init, my assembly, console test app, basic types

---
+++ AdvancedC#/ReflectionStudy/MyAssembly/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssembly
+{
+    public class Circle : Figure, IFigure
+    {
+        public double Area()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdvancedC#/ReflectionStudy/MyAssembly/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssembly
+{
+    public interface IFigure
+    {
+        double Area();
+        double Perimeter();
+        void Display();
+    }
+}
