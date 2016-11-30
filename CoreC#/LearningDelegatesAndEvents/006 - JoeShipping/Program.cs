022c8d5eab633a0f81c10411c94365098228ea63
aperepeliak <aperepelyak@gmail.com>
2016-11-30 10:42:04 +0200

    complete mini-challenge on delegates

---
+++ CoreC#/LearningDelegatesAndEvents/006 - JoeShipping/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006___JoeShipping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShippingFeesDelegate feeDelegate;
+            ShippingDestination destination;
+            string zone;
+
+            do
+            {
+                Console.Write("Enter the destination -> ");
+                zone = Console.ReadLine();
+
+                if (zone != "exit")
+                {
+                    destination = ShippingDestination.getDestinationInfo(zone);
+
+                    if (destination != null)
+                    {
+                        Console.Write("   > Enter item price -> ");
+                        string priceStr = Console.ReadLine();
+                        decimal price = decimal.Parse(priceStr);
+
+                        feeDelegate = destination.calcFees;
+
+                        decimal finalFee = 0.0m;
+                        feeDelegate(price, ref finalFee);
+                        Console.WriteLine($"   > The shipping fee is: {finalFee}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("   > Unknown destination. Try again or type 'exit'");
+                    }
+                }
+            } while (zone != "exit");
+        }
+    }
+}
---
+++ CoreC#/LearningDelegatesAndEvents/006 - JoeShipping/ShippingDestination.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006___JoeShipping
+{
+
+    public delegate void ShippingFeesDelegate(decimal price, ref decimal fee);
+
+    abstract class ShippingDestination
+    {
+        public bool isHighRisk { get; set; }
+        public decimal FeeRate { get; }
+
+        public ShippingDestination(decimal rate, bool risk = false)
+        {
+            FeeRate = rate;
+            isHighRisk = risk;
+        }
+
+        public virtual void calcFees(decimal price, ref decimal fee)
+        {
+            fee = price * FeeRate;
+
+            if (isHighRisk)
+            {
+                fee += 25.0m;
+            }
+        }
+
+        public static ShippingDestination getDestinationInfo(string dest)
+        {
+            if (dest == "zone1")
+                return new Zone1();
+
+            if (dest == "zone2")
+                return new Zone2();
+
+            if (dest == "zone3")
+                return new Zone3();
+
+            if (dest == "zone4")
+                return new Zone4();
+
+            return null;
+        }
+    }
+
+    class Zone1 : ShippingDestination
+    {
+        public Zone1() : base(0.25m) { }
+    }
+
+    class Zone2 : ShippingDestination
+    {
+        public Zone2() : base(0.12m, true) { }
+    }
+
+    class Zone3 : ShippingDestination
+    {
+        public Zone3() : base(0.08m) { }
+    }
+
+    class Zone4 : ShippingDestination
+    {
+        public Zone4() : base(0.04m, true) { }
+    }
+}
