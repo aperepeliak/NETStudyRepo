36548f395a80c7598a4331a4bfeab749d546ad95
aperepeliak <aperepelyak@gmail.com>
2016-12-02 20:45:54 +0200

    learning anonymous types

---
+++ CoreC#/OtherFeatures/003 - AnonymousTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003___AnonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void BuildAnonType(string make, string color, int currSp)
+        {
+            // Anonymous type definition
+            var car = new { Make = make, Color = color, Speed = currSp };
+
+            // Anonymous type usage
+            Console.WriteLine($"You have a {car.Color} {car.Make} going {car.Speed} MPH");
+
+        }
+    }
+}
