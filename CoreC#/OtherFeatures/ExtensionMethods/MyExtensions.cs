976e664259f84338f2876e6941e69d304eedd19c
aperepeliak <aperepelyak@gmail.com>
2016-12-02 15:01:16 +0200

    learning extension methods

---
+++ CoreC#/OtherFeatures/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace MyExt
+{
+    static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine($"{obj.GetType().Name} lives here: => {Assembly.GetAssembly(obj.GetType()).GetName().Name}");
+        }
+
+        public static int ReverseDigits(this int i)
+        {
+            // 1) int => string => char[]
+            char[] digits = i.ToString().ToCharArray();
+
+            // 2) reversing
+            Array.Reverse(digits);
+
+            // 3) back to string
+            string newDigits = new string(digits);
+
+            // 4) bacl to int
+            return int.Parse(newDigits);
+        }
+    }
+}
---
+++ CoreC#/OtherFeatures/ExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with extension methods\n\n");
+
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            int reversed = myInt.ReverseDigits();
+            Console.WriteLine($"reversed: {reversed}");
+        }
+    }
+}
