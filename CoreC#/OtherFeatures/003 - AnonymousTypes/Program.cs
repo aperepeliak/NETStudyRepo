4f6d5dad894588c7f38c5c80f53a12e6d3a20cc5
aperepeliak <aperepelyak@gmail.com>
2016-12-04 13:29:51 +0200

    learning anon types comparison, reflection

--- CoreC#/OtherFeatures/003 - AnonymousTypes/Program.cs
+++ CoreC#/OtherFeatures/003 - AnonymousTypes/Program.cs
@@ -10,6 +10,25 @@ namespace _003___AnonymousTypes
     {
         static void Main(string[] args)
         {
+            var myCar = new { Color = "Bright Pink", Make = "Saab", CurrentSpeed = 55 };
+            Console.WriteLine($"MyCar: {myCar.Color} {myCar.Make}");
+
+            BuildAnonType("BMW", "Black", 60);
+            Console.WriteLine("-------------");
+
+            ReflectOverAnonymousType(myCar);
+            EqualityTest();
+
+
+            // Composite anonymous types
+            var purchaseItem = new
+            {
+                TimeBought = DateTime.Now,
+                ItemBought = new { Color = "Red", Make = "Saab", CurrentSpeed = "55" },
+                Price = 34.000
+            };
+
+            ReflectOverAnonymousType(purchaseItem);
         }
 
         static void BuildAnonType(string make, string color, int currSp)
@@ -19,7 +38,49 @@ namespace _003___AnonymousTypes
 
             // Anonymous type usage
             Console.WriteLine($"You have a {car.Color} {car.Make} going {car.Speed} MPH");
+        }
 
+        static void ReflectOverAnonymousType(object obj)
+        {
+            Console.WriteLine($"obj is an instance of: {obj.GetType().Name}");
+
+            Console.WriteLine("Base class of {0} is {1}",
+                obj.GetType().Name,
+                obj.GetType().BaseType);
+
+            Console.WriteLine($"obj.ToString() == {obj.ToString()}");
+            Console.WriteLine($"obj.GetHashCode() == {obj.GetHashCode()}");
+            Console.WriteLine("--------------------");
+        }
+
+        static void EqualityTest()
+        {
+            Console.WriteLine("\n***** Equality test method START *****\n");
+            var car1 = new { Color = "Bright Pink", Make = "Saab", CurrentSpeed = 55 };
+            var car2 = new { Color = "Bright Pink", Make = "Saab", CurrentSpeed = 55 };
+
+            // Are they equal using Equals()?
+            if (car1.Equals(car2))
+                Console.WriteLine("Same anonymous objects! (EQUALS)");
+            else
+                Console.WriteLine("NOT the same! (EQUALS)");
+
+            // Are they equal using '=='?
+            if (car1 == car2)
+                Console.WriteLine("Same anonymous objects! ('==')");
+            else
+                Console.WriteLine("NOT the same! (==)");
+
+            // Are these objects the same underlying type?
+            if (car1.GetType().Name == car2.GetType().Name)
+                Console.WriteLine("We are both the same type!");
+            else
+                Console.WriteLine("We are different types!");
+
+            Console.WriteLine("========================");
+            ReflectOverAnonymousType(car1);
+            ReflectOverAnonymousType(car2);
+            Console.WriteLine("\n***** Equality test method END *****\n");
         }
     }
 }
