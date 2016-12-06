ae4f00a09b4212b0db01b8b332c6bcae9c4123ba
aperepeliak <aperepelyak@gmail.com>
2016-12-06 08:19:21 +0200

    learning linq as return value

---
+++ LINQ/LINQtoObjects/002-LinqRetValues/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_LinqRetValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Return values\n");
+            IEnumerable<string> subset = GetStringSubset();
+
+            foreach (string item in subset)
+                Console.WriteLine(item);
+
+            Console.WriteLine("######");
+            foreach(var item in GetStringSubsetAsArray())
+                Console.WriteLine(item);
+
+        }
+
+        private static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = { "Yellow", "Blue", "Green", "Black", "Pink", "Dark Red", "Light Red" };
+
+            IEnumerable<string> theRedColors = from c in colors
+                                               where c.Contains("Red")
+                                               select c;
+
+            return theRedColors;
+        }
+
+        // Using immediate execution
+        static string[] GetStringSubsetAsArray()
+        {
+            string[] colors = { "Yellow", "Blue", "Green", "Black", "Pink", "Dark Red", "Light Red" };
+            var theRedColors = from c in colors
+                               where c.Contains("Red")
+                               select c;
+
+            return theRedColors.ToArray();   
+        }
+    }
+}
