acaa52b2c369b52ef55c85944be4a1e4a208c39e
aperepeliak <aperepelyak@gmail.com>
2016-12-08 21:34:09 +0200

    learning filtering data

---
+++ LINQ/LINQFundamentals/004-OrderFilterProject/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_OrderFilterProject
+{
+    public class Car
+    {
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{Year,-5} {Manufacturer, -30} {Name, -50} {Displacement, -5} {Cylinders,-4} {City, -4} {Highway, -4} {Combined, -4}");
+        }
+
+        public static Car ParseFromCsv(string line)
+        {
+            var columns = line.Split(',');
+
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = double.Parse(columns[3], CultureInfo.InvariantCulture),
+                Cylinders = int.Parse(columns[4]),
+                City = int.Parse(columns[5]),
+                Highway = int.Parse(columns[6]),
+                Combined = int.Parse(columns[7])
+            };
+        }
+    }
+}
---
+++ LINQ/LINQFundamentals/004-OrderFilterProject/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_OrderFilterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            // Show all
+            //foreach (var car in cars)
+            //    Console.WriteLine($"{car}");
+
+            Console.WriteLine("Car with the best fuel efficiency");
+            var fuelEffQuery = cars.OrderByDescending(c => c.Combined)
+                                    .ThenBy(c => c.Name);
+
+            // same query syntax
+            var fuelEffQuery2 = from car in cars
+                                orderby car.Combined descending, car.Name ascending
+                                select car;
+
+            foreach (var car in fuelEffQuery.Take(10))
+                Console.WriteLine(car);
+            Console.WriteLine(new string('*', 110));
+
+            var result = cars.Any(c => c.Manufacturer == "Ford");
+            Console.WriteLine($"Is there any manufacturer Ford: {result}");
+
+            var result2 = cars.All(c => c.Manufacturer == "Ford");
+            Console.WriteLine($"Do all of the cars are Ford: {result2}");
+
+            Console.WriteLine(new string('*', 110));
+
+            var charactersQuery = cars.SelectMany(c => c.Name)
+                .OrderBy(c => c)
+                .Distinct();
+
+            foreach (var character in charactersQuery)
+            {
+                Console.Write(character);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            // Query syntax
+            return
+                (from line in File.ReadAllLines(path).Skip(1)
+                 where line.Length > 1
+                 select Car.ParseFromCsv(line))
+                .ToList();
+
+            // Method syntax
+
+            //File.ReadAllLines(path)
+            //.Skip(1)
+            //.Where(line => line.Length > 1)
+            //.Select(Car.ParseFromCsv)
+            //.ToList();
+        }
+    }
+}
