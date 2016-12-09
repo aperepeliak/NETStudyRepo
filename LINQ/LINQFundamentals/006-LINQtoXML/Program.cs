951175e9c92794fc8f3160704ec33934ab88072c
aperepeliak <aperepelyak@gmail.com>
2016-12-09 21:35:29 +0200

    learning Linq to xml

---
+++ LINQ/LINQFundamentals/006-LINQtoXML/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Linq;
+
+namespace _006_LINQtoXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateXML();
+
+
+        }
+
+        private static void CreateXML()
+        {
+            var records = ProcessCars("fuel.csv");
+
+            var document = new XDocument();
+            var cars = new XElement("Cars",
+
+                from record in records
+                select new XElement("Car",
+                        new XAttribute("Name", record.Name),
+                        new XAttribute("Combined", record.Combined),
+                        new XAttribute("Manufacturer", record.Manufacturer))
+            );
+
+            document.Add(cars);
+            document.Save("fuel.xml");
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+
+            var query = File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(l => l.Length > 1)
+                            .ToCar();
+
+            return query.ToList();
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3], CultureInfo.InvariantCulture),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+    }
+}
