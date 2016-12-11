b80d1eba886947838df2cc56f9c5a9093292ae88
aperepeliak <aperepelyak@gmail.com>
2016-12-11 16:08:25 +0200

    querying mssql using linq and EF

---
+++ LINQ/LINQFundamentals/LINQandEF/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQandEF
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Displacement { get; set; }
+        public int Cylinders { get; set; }
+        public int City { get; set; }
+        public int Highway { get; set; }
+        public int Combined { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"{Manufacturer, -30} {Name, -50} {Displacement, -5} {Cylinders,-4} {City, -4} {Highway, -4} {Combined, -4}");
+        }
+    }
+}
---
+++ LINQ/LINQFundamentals/LINQandEF/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQandEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<CarDb>());
+            InsertData();
+            QueryData();
+        }
+
+        private static void QueryData()
+        {
+            // IQueryable can be translated by EF into efficeint SQL
+            // IEnumerable operation always has to happen in memory
+
+            var db = new CarDb();
+            // db.Database.Log = Console.WriteLine;
+
+            var query = from car in db.Cars
+                        orderby car.Combined descending, car.Name ascending
+                        select car;
+
+            var query2 = db.Cars
+                 .GroupBy(c => c.Manufacturer)
+                 .Select(g => new
+                 {
+                     Name = g.Key,
+                     Cars = g.OrderByDescending(c => c.Combined).Take(2)
+                 });
+
+            foreach (var group in query2)
+            {
+                Console.WriteLine(group.Name);
+                foreach (var car in group.Cars)
+                {
+                    Console.WriteLine($"\t{car}");
+                }
+            }
+
+
+
+        }
+
+        private static void InsertData()
+        {
+            var cars = ProcessCars("fuel.csv");
+            var db = new CarDb();
+
+            if (!db.Cars.Any())
+            {
+                foreach (var car in cars)
+                {
+                    db.Cars.Add(car);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        private static List<Car> ProcessCars(string path)
+        {
+            var query = File.ReadAllLines(path)
+                            .Skip(1)
+                            .Where(l => l.Length > 1)
+                            .ToCar();
+
+            return query.ToList();
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(',');
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3], CultureInfo.InvariantCulture),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
+        }
+    }
+}
