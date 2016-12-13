a43561ab537f6b74a28f46d7433567595aeb2fd5
aperepeliak <aperepelyak@gmail.com>
2016-12-13 11:03:02 +0200

    add client app for assembly test

---
+++ AdvancedC#/NET_Assemblies/Assemblies-BuildingAndConfig/CarClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CarLibrary;
+
+namespace CarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** CarLibrary Client App ****");
+
+            SportsCar viper = new SportsCar("Viper", 240, 40);
+            var mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Press any key to terminate.");
+        }
+    }
+}
