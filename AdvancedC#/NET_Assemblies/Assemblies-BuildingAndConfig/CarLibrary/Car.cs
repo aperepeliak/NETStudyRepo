bd6e15aa2ebb48716429f8d9842c9ad8e073cc5c
aperepeliak <aperepelyak@gmail.com>
2016-12-13 09:31:57 +0200

    add CarLib.dll

---
+++ AdvancedC#/NET_Assemblies/Assemblies-BuildingAndConfig/CarLibrary/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace CarLibrary
+{
+    public enum EngineState { engineAlive, engineDead }
+
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState
+        {
+            get { return egnState; }
+        }
+
+        public abstract void TurboBoost();
+
+        public Car() { }
+        public Car(string name, int mxSp, int currSp)
+        {
+            PetName = name;
+            MaxSpeed = mxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+}
---
+++ AdvancedC#/NET_Assemblies/Assemblies-BuildingAndConfig/CarLibrary/DerivedCars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    public class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int mxSp, int currSp)
+            : base (name, mxSp, currSp) { }
+
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Ramming speed!", "Faster is better...");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan () { }
+        public MiniVan(string name, int mxSp, int currSp)
+            : base(name, mxSp, currSp) { }
+
+        public override void TurboBoost()
+        {
+            egnState = EngineState.engineDead;
+            MessageBox.Show("Eek!", "Your engine block exploded!");
+        }
+    }
+}
