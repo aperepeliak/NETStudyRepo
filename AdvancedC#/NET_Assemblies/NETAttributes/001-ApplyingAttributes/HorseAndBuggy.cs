395ff340b8ebd129d9dcdcc462209837e66bc7b1
aperepeliak <aperepelyak@gmail.com>
2016-12-15 09:47:03 +0200

    learning .NET attributes

---
+++ AdvancedC#/NET_Assemblies/NETAttributes/001-ApplyingAttributes/HorseAndBuggy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ApplyingAttributes
+{
+
+    // Example of multiple attributes
+    // Alternatively, we could type 
+    // [Serializable]
+    // [Obsolete("Just do not use")]
+
+    [Serializable, Obsolete("Use another vehicle!")]
+    public class HorseAndBuggy
+    {
+        // ...
+    }
+}
---
+++ AdvancedC#/NET_Assemblies/NETAttributes/001-ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_ApplyingAttributes
+{
+    [Serializable]
+    public class Motorcycle
+    {
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+
+        // These fields are still serializable
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+}
