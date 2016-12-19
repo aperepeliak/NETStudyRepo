cb0af8c3f5486629527b8dec0146437762f160ce
aperepeliak <aperepelyak@gmail.com>
2016-12-19 19:24:10 +0200

    learning lock keyword

---
+++ AdvancedC#/Multithreading/MultithreadSync/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadSync
+{
+    public class Printer
+    {
+        // Lock token
+        private object threadLock = new object();
+
+        public void PrintNumbers()
+        {
+
+            lock (threadLock)
+            {
+                Console.WriteLine($" -> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+                Console.Write("Your numbers: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    Random r = new Random();
+                    Thread.Sleep(1000 * r.Next(5));
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AdvancedC#/Multithreading/MultithreadSync/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer p = new Printer();
+
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(p.PrintNumbers);
+                threads[i].Name = string.Format($"Worker thread №{i}");
+            }
+
+            foreach (var t in threads)
+            {
+                t.Start();
+            }
+        }
+    }
+}
