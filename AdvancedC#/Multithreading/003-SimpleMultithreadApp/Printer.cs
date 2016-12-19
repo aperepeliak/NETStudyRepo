8b1bbcc2b589f5ee472b828753a84242cf4aecb6
aperepeliak <aperepelyak@gmail.com>
2016-12-19 17:43:35 +0200

    add simple multithread app

---
+++ AdvancedC#/Multithreading/003-SimpleMultithreadApp/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _003_SimpleMultithreadApp
+{
+    public class Printer
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine($" -> {Thread.CurrentThread.Name} is executing PrintNumbers()");
+
+            Console.Write("Your numbers: ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{i}, ");
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AdvancedC#/Multithreading/003-SimpleMultithreadApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _003_SimpleMultithreadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Thread APP *****\n");
+            Console.Write("Do you want [1] or [2] threads? -> ");
+            string threadCount = Console.ReadLine();
+
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            Console.WriteLine($" -> {Thread.CurrentThread.Name} is executing Main()");
+
+            Printer p = new Printer();
+
+            switch(threadCount)
+            {
+                case "2":
+                    Thread bgThread = new Thread(p.PrintNumbers);
+                    bgThread.Name = "Secondary";
+                    bgThread.Start();
+                    break;
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("I don't know what you want ... you get 1 thread");
+                    goto case "1";
+            }
+            System.Windows.Forms.MessageBox.Show("I'm busy!", "Work on main thread ...");
+        }
+    }
+}
