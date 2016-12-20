5ccf7b1b882d80e09ee24b7d1237672991347e5c
aperepeliak <aperepelyak@gmail.com>
2016-12-20 08:18:04 +0200

    learning timer callbacks

---
+++ AdvancedC#/Multithreading/004-TimerApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _004_TimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTimer = new Timer((state) =>
+            { 
+                Console.WriteLine($"Time is: {DateTime.Now.ToLongTimeString()} \tState: {state.ToString()}");
+            },
+            "Hello from Main!",
+            0, 1000);
+
+            Console.WriteLine("Hit key to terminate ...");
+            Console.ReadKey();
+        }
+    }
+}
