5267c371032219a46817541a22d6a9ae55743671
aperepeliak <aperepelyak@gmail.com>
2016-12-20 20:40:53 +0200

    add data parallelism with foreach

---
+++ AdvancedC#/Multithreading/005-DataParallelismWithForEach/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _005_DataParallelismWithForEach
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnProcessImages = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 25);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(260, 178);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnProcessImages
+            // 
+            this.btnProcessImages.Location = new System.Drawing.Point(178, 226);
+            this.btnProcessImages.Name = "btnProcessImages";
+            this.btnProcessImages.Size = new System.Drawing.Size(93, 23);
+            this.btnProcessImages.TabIndex = 1;
+            this.btnProcessImages.Text = "Flip Images";
+            this.btnProcessImages.UseVisualStyleBackColor = true;
+            this.btnProcessImages.Click += new System.EventHandler(this.btnProcessImages_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(12, 226);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Type here while images are being processed ...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnProcessImages);
+            this.Controls.Add(this.txtInput);
+            this.Name = "MainForm";
+            this.Text = "TPL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnProcessImages;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AdvancedC#/Multithreading/005-DataParallelismWithForEach/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _005_DataParallelismWithForEach
+{
+    public partial class MainForm : Form
+    {
+
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcessImages_Click(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                ProcessFiles();
+            });
+        }
+
+        private void ProcessFiles()
+        {
+            // Using ParallelOptions instance to store the CancellationToken
+            var parOpts = new ParallelOptions();
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+
+            // Load up all *.jpg files, and make a new folder for the  modified data
+            string[] files = Directory.GetFiles(
+                @"D:\Study\NETStudyRepo\files\troelsenJapikse\Chapter_19\TestPictures",
+                "*.jpg", SearchOption.AllDirectories);
+
+            string newDir = @"D:\ModifiedPictures";
+            Directory.CreateDirectory(newDir);
+            try
+            {
+                Parallel.ForEach(files, currFile =>
+                {
+                    string filename = Path.GetFileName(currFile);
+                    using (var bitmap = new Bitmap(currFile))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, filename));
+
+                        this.Invoke((Action)delegate
+                        {
+                            this.Text =
+                            string.Format($"Processing {filename} on thread {Thread.CurrentThread.ManagedThreadId}");
+                        });
+                    }
+                });
+                this.Invoke((Action)delegate
+                {
+                    this.Text = "Done!";
+                });
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action)delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }
+    }
+}
