d15243d14b3dee1c5db46b8ce6a82beeaf55b770
aperepeliak <aperepelyak@gmail.com>
2016-12-22 11:17:12 +0200

    exploring File and FileInfo classes

---
+++ AdvancedC#/FileIO/003-SimpleFileIO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_SimpleFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myTasks =
+            {
+                "Learn ASP.NET MVC",
+                "Learn WCF",
+                "Learn JS",
+                "Study C#"
+            };
+
+            File.WriteAllLines(@"D:\tasks.txt", myTasks);
+
+            foreach (var task in File.ReadAllLines(@"D:\tasks.txt"))
+            {
+                Console.WriteLine($"TODO: {task}");
+            }
+        }
+    }
+}
