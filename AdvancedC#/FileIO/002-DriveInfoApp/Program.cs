00a78761ec438b65aabce5cf0f56153f6c519794
aperepeliak <aperepelyak@gmail.com>
2016-12-22 09:48:38 +0200

    exploring DriveInfo class

---
+++ AdvancedC#/FileIO/002-DriveInfoApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_DriveInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDrives = DriveInfo.GetDrives();
+            foreach (var drive in myDrives)
+            {
+                Console.WriteLine($"{"Name", -12} : {drive.Name}");
+                Console.WriteLine($"{"Type", -12} : {drive.DriveType}");
+
+                // Check to see whether the drive is mounted
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"{"Free space", -12} : {drive.TotalFreeSpace / 1024 / 1024, 0:N0} MB");
+                    Console.WriteLine($"{"Format",     -12} : {drive.DriveFormat}");
+                    Console.WriteLine($"{"Label",      -12} : {drive.VolumeLabel}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
