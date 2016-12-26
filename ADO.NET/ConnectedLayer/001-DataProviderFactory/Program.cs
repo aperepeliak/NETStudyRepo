50855f32306f26156152d154bc974287ec930b15
aperepeliak <aperepelyak@gmail.com>
2016-12-26 09:21:42 +0200

    add localdb AutoLot and test connection with data provider factory model

---
+++ ADO.NET/ConnectedLayer/001-DataProviderFactory/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data.Common;
+
+namespace _001_DataProviderFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataProvider = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
+
+            using (DbConnection conn = factory.CreateConnection())
+            {
+                if (conn == null)
+                {
+                    ShowError("Connection");
+                    return;
+                }
+                Console.WriteLine($"Your connection object is a: {conn.GetType().Name}");
+                conn.ConnectionString = connectionString;
+                conn.Open();
+
+                DbCommand command = factory.CreateCommand();
+                if (command == null)
+                {
+                    ShowError("Command");
+                    return;
+                }
+                Console.WriteLine($"Your command object is a: {command.GetType().Name}");
+                command.Connection = conn;
+                command.CommandText = "Select * from Inventory";
+
+                // Print data with DataReader
+                using (DbDataReader dataReader = command.ExecuteReader())
+                {
+                    Console.WriteLine($"Your data reader object is a: {dataReader.GetType().Name}");
+                    Console.WriteLine("\nCurrent Inventory");
+                    while (dataReader.Read())
+                    {
+                        Console.WriteLine($" -> Car #{dataReader["CarId"]} is a {dataReader["Make"]}.");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static void ShowError(string objectName)
+        {
+            Console.WriteLine($"There was an issue creating the {objectName}");
+            Console.ReadLine();
+        }
+    }
+}
