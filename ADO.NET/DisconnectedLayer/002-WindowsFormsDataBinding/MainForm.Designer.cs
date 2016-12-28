f70972c898b99cd937a40c547f4912eeac9f6fff
aperepeliak <aperepelyak@gmail.com>
2016-12-28 15:57:46 +0200

    bind DataTable to dataGridView control

---
+++ ADO.NET/DisconnectedLayer/002-WindowsFormsDataBinding/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace _002_WindowsFormsDataBinding
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.carInvGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.carInvGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(263, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Here is what we have in stock";
+            // 
+            // carInvGridView
+            // 
+            this.carInvGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.carInvGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.carInvGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.carInvGridView.Location = new System.Drawing.Point(18, 54);
+            this.carInvGridView.Name = "carInvGridView";
+            this.carInvGridView.Size = new System.Drawing.Size(612, 271);
+            this.carInvGridView.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 337);
+            this.Controls.Add(this.carInvGridView);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "Windows Forms Data Binding";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.carInvGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView carInvGridView;
+    }
+}
+
---
+++ ADO.NET/DisconnectedLayer/002-WindowsFormsDataBinding/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _002_WindowsFormsDataBinding
+{
+    public partial class MainForm : Form
+    {
+        List<Car> listCars = null;
+        DataTable invTable = new DataTable();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            listCars = new List<Car>
+            {
+                new Car {Id = 1, PetName = "Chucky", Make = "BMW", Color = "Black" },
+                new Car {Id = 2, PetName = "Dizzy", Make = "Saab", Color = "Green" },
+                new Car {Id = 3, PetName = "Charlie", Make = "ZAZ", Color = "Pink" },
+                new Car {Id = 4, PetName = "Bobby", Make = "Ford", Color = "Navy" },
+                new Car {Id = 5, PetName = "Sam", Make = "GM", Color = "White" }
+            };
+
+            CreateDataTable();
+        }
+
+        private void CreateDataTable()
+        {
+            var carIDColumn = new DataColumn("Id", typeof(int));
+            var carPetNameColumn = new DataColumn("PetName", typeof(string)) { Caption = "Pet Name" };
+            var carMakeColumn = new DataColumn("Make", typeof(string));
+            var carColorColumn = new DataColumn("Color", typeof(string));
+
+            invTable.Columns.AddRange(new[] { carIDColumn, carPetNameColumn, carMakeColumn, carColorColumn });
+
+            foreach (var c in listCars)
+            {
+                var newRow = invTable.NewRow();
+                newRow["Id"] = c.Id;
+                newRow["Make"] = c.Make;
+                newRow["Color"] = c.Color;
+                newRow["PetName"] = c.PetName;
+                invTable.Rows.Add(newRow);
+            }
+
+            carInvGridView.DataSource = invTable;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
