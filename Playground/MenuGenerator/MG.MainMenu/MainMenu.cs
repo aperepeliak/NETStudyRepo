b9d714258d76847cfeac86e83a5236b67e7ba51c
aperepeliak <aperepelyak@gmail.com>
2017-01-02 21:38:03 +0200

    add get data from xml method

--- Playground/MenuGenerator/MG.MainMenu/MainMenu.cs
+++ Playground/MenuGenerator/MG.MainMenu/MainMenu.cs
@@ -11,6 +11,7 @@ using MG.Entities;
 using System.Xml.Linq;
 using System.IO;
 using System.Xml.Serialization;
+using System.Globalization;
 
 namespace MG.MainMenu
 {
@@ -26,35 +27,40 @@ namespace MG.MainMenu
             if (!File.Exists("Recipes.xml"))
             {
                 recipes = SeedData();
-                var document = new XDocument();
-
-                var rcps = new XElement("Recipes");
-
-                var elements =
-                    from r in recipes
-                    select new XElement("Recipe",
-                        new XAttribute("Name", r.Name),
-                        new XAttribute("Category", r.DishCategory.Name),
-                        new XAttribute("Seasonality", r.Seasonality.Name),
-
-                        new XElement("Ingredients",
-                            from i in r.Ingredients
-                            select new XElement
-                                ("IngredientInfo",
-                                    new XElement
-                                        ("Ingredient",
-                                    new XAttribute("Name", i.Ingredient.Name),
-                                    new XAttribute("Units", i.Ingredient.Units.Name)
-                                        ),
-                            new XElement("Amount", i.Amount)
-                                )
-                            )   
-                        );
-
-                rcps.Add(elements);
-                document.Add(rcps);
-                document.Save("Recipes.xml");
+                CreateXML();
             }
+        }
+
+        private void CreateXML()
+        {
+            var document = new XDocument();
+
+            var rcps = new XElement("Recipes");
+
+            var elements =
+                from r in recipes
+                select new XElement("Recipe",
+                    new XAttribute("Name", r.Name),
+                    new XAttribute("Category", r.DishCategory.Name),
+                    new XAttribute("Seasonality", r.Seasonality.Name),
+
+                    new XElement("Ingredients",
+                        from i in r.Ingredients
+                        select new XElement
+                            ("IngredientInfo",
+                                new XElement
+                                    ("Ingredient",
+                                new XAttribute("Name", i.Ingredient.Name),
+                                new XAttribute("Units", i.Ingredient.Units.Name)
+                                    ),
+                        new XElement("Amount", i.Amount)
+                            )
+                        )
+                    );
+
+            rcps.Add(elements);
+            document.Add(rcps);
+            document.Save("Recipes.xml");
         }
 
         private List<Recipe> SeedData()
@@ -64,7 +70,7 @@ namespace MG.MainMenu
             var allSeason = new Season() { Name = "AllSeason" };
 
             var firstDishes = new Category() { Name = "FirstDishes" };
-            var secondDishes = new Category() {Name = "SecondDishes" };
+            var secondDishes = new Category() { Name = "SecondDishes" };
 
             var kilos = new Unit() { Name = "кг" };
             var litres = new Unit() { Name = "л" };
@@ -122,24 +128,41 @@ namespace MG.MainMenu
                     throw new Exception("У вас пока не добавлено ни одного рецепта!");
                 else
                 {
-                    var query =
-                        from recipe in document.Element("Recipes").Elements("Recipe")
-                        select new Recipe
-                        {
-                            Name = recipe.Name.ToString(),
-                            Seasonality = new Season() { Name = recipe.Attribute("Seasonality").Value },
-                             DishCategory = new Category() { Name = recipe.Attribute("DishCategory").Value }
-                        };
-
-                     recipes = query.ToList();
-
+                    recipes = GetDataFromXml(document);
+                    MessageBox.Show(recipes[1].Name);
                 }
-                    
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Ошибка");
             }
+        }
+
+        private List<Recipe> GetDataFromXml(XDocument document)
+        {
+            var query =
+                        from recipe in document.Element("Recipes").Elements("Recipe")
+                        select new Recipe
+                        {
+                            Name = recipe.Attribute("Name").Value,
+                            Seasonality = new Season() { Name = recipe.Attribute("Seasonality").Value },
+                            DishCategory = new Category() { Name = recipe.Attribute("Category").Value },
+                            Ingredients =
+                                (from ingrInfo in recipe.Element("Ingredients").Elements("IngredientInfo")
+                                 select new IngredientInfo()
+                                 {
+                                     Ingredient = new Ingredient()
+                                     {
+                                         Name = ingrInfo.Element("Ingredient").Attribute("Name").Value,
+                                         Units = new Unit() { Name = ingrInfo.Element("Ingredient").Attribute("Units").Value }
+                                     },
+                                     Amount = double.Parse(ingrInfo.Element("Amount").Value, CultureInfo.InvariantCulture)
+                                 }).ToList()
+                        };
+
+            return query.ToList();
+
         }
     }
 }
