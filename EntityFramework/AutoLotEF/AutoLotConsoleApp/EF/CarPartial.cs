b2fa6894175e774c31fbbd67d8eb8df919bc7fd5
aperepeliak <aperepelyak@gmail.com>
2017-01-03 11:23:34 +0200

    add car partial class and rename some entities and props

---
+++ EntityFramework/AutoLotEF/AutoLotConsoleApp/EF/CarPartial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoLotConsoleApp.EF
+{
+    public partial class Car
+    {
+        public override string ToString()
+        {
+            return $"{this.CarNickName ?? "**No Name**"} is a {this.Color} {this.Make} with ID {this.CarId}";
+        }
+    }
+}
