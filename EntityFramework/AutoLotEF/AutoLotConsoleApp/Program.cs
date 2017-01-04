93881d6eaa2d059e5827a44a825a6f02f5bbfc9c
aperepeliak <aperepelyak@gmail.com>
2017-01-04 10:17:39 +0200

    learning basic CRUD ops with EF

--- EntityFramework/AutoLotEF/AutoLotConsoleApp/Program.cs
+++ EntityFramework/AutoLotEF/AutoLotConsoleApp/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using AutoLotConsoleApp.EF;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace AutoLotConsoleApp
 {
@@ -12,8 +14,70 @@ namespace AutoLotConsoleApp
     {
         static void Main(string[] args)
         {
-            PrintAllInventorySQL();
+            UpdateRecord(15);
+
             Console.ReadLine();
+        }
+
+        private static void UpdateRecord(int carId)
+        {
+            using (var context = new AutoLotEntities())
+            {
+                var carToUpdate = context.Cars.Find(carId);
+
+                if (carToUpdate != null)
+                {
+                    Console.WriteLine(context.Entry(carToUpdate).State);
+                    carToUpdate.Color = "Magento";
+                    Console.WriteLine(context.Entry(carToUpdate).State);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static void RemoveRecordUsingEntityState(int carId)
+        {
+            using (var context = new AutoLotEntities())
+            {
+                Car carToDelete = new Car { CarId = carId };
+                context.Entry(carToDelete).State = EntityState.Deleted;
+
+                try
+                {
+                    context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        private static void RemoveRecord(int carId)
+        {
+            using (var context = new AutoLotEntities())
+            {
+                var carToDelete = context.Cars.Find(carId);
+                if (carToDelete != null)
+                {
+                    context.Cars.Remove(carToDelete);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private static void PrintInventoriesLinq()
+        {
+            using (var context = new AutoLotEntities())
+            {
+                var query = context.Cars
+                    .Where(c => c.Make == "ZAZ");
+
+                foreach (var c in query)
+                {
+                    Console.WriteLine(c);
+                }
+            }
         }
 
         private static void PrintAllInventory()
@@ -27,7 +91,7 @@ namespace AutoLotConsoleApp
             }
         }
 
-        private static void PrintAllInventorySQL()
+        private static void PrintInventorySQL()
         {
             using (var context = new AutoLotEntities())
             {
