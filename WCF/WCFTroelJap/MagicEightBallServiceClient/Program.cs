c9267f7223a0bac48ece8a2e5f24fd3a6edaf4fd
aperepeliak <aperepelyak@gmail.com>
2017-01-10 15:08:34 +0200

    add service client

---
+++ WCF/WCFTroelJap/MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MagicEightBallServiceClient.ServiceReference;
+
+namespace MagicEightBallServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Ask the Magic Ball *****\n");
+
+            using (var magicBall = new EightBallClient())
+            {
+                Console.Write("Your question -> ");
+                var question = Console.ReadLine();
+                var answer = magicBall.ObtainAnswerToQuestion(question);
+
+                Console.WriteLine($"The ball says: {answer}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
