23e1a6741b9d9485482a17eb03a74aa61f0fce95
aperepeliak <aperepelyak@gmail.com>
2017-01-12 12:13:44 +0200

    add math service lib

---
+++ WCF/WCFTroelJap/MathWindowsServiceHost/MathWinService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using MathServiceLibrary;
+using System.ServiceModel;
+
+namespace MathWindowsServiceHost
+{
+    public partial class MathWinService : ServiceBase
+    {
+        private ServiceHost myHost;
+
+        public MathWinService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (myHost != null)
+            {
+                myHost.Close();
+                myHost = null;
+            }
+
+            myHost = new ServiceHost(typeof(MathService));
+
+            Uri address = new Uri("http://localhost:8080/MathServiceLibrary");
+            WSHttpBinding binding = new WSHttpBinding();
+            Type contract = typeof(IBasicMath);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
