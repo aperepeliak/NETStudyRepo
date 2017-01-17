071589ec272c252e4af621e7fdca830badec0191
aperepeliak <aperepelyak@gmail.com>
2017-01-17 10:09:23 +0200

    add codeBehindPageModel

---
+++ ASP.NET/ASPTroelJap/SinglePageModel/CodeBehindPageModel/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+
+namespace CodeBehindPageModel
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        public IEnumerable<Inventory> GetData()
+        {
+            Trace.Write("Default.aspx", "Getting Data");
+            return new InventoryRepo().GetAll();
+        }
+    }
+}
