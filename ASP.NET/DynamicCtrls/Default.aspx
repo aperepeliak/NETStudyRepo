27d7eed64535856272ecbfb491348d3c900737ad
aperepeliak <aperepelyak@gmail.com>
2017-01-20 10:19:15 +0200

    dynamic ctrls

---
+++ ASP.NET/DynamicCtrls/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Dynamic Controls</h1>
+            <asp:Panel ID="myPanel" runat="server" BorderColor="Pink" BorderStyle="Solid" Width="300px">
+                <br /><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />
+                <asp:Button ID="Button1" runat="server" Text="Button" /><br /><br />
+                <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink><br /><br />
+            </asp:Panel>
+            <br />
+            <br />
+
+            <asp:Button ID="btnAddWidgets" runat="server" Text="Add widgets" OnClick="btnAddWidgets_Click" />
+            <asp:Button ID="btnClearPanel" runat="server" Text="Clear Panel" OnClick="btnClearPanel_Click" />
+            <br />
+            
+            <asp:Button ID="btnGetTxtData" runat="server" Text="Get Txt Data" OnClick="btnGetTxtData_Click" />
+            <br />
+            <asp:Label ID="lblTextBoxData" runat="server" Text=""></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="lblControlInfo" runat="server" Text="[result]"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET/DynamicCtrls/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        ListControlsInPanel();
+    }
+
+    private void ListControlsInPanel()
+    {
+        var theInfo = string.Empty;
+
+        theInfo = $"<b>Does the panel have controls? {myPanel.HasControls()}</b><br/>";
+
+        foreach (Control c in myPanel.Controls)
+        {
+            if (!object.ReferenceEquals(c.GetType(), typeof(System.Web.UI.LiteralControl)))
+            {
+                theInfo += $"*********************<br/>";
+                theInfo += $"Control name? {c} <br/>";
+                theInfo += $"ID? {c.ID} <br/>";
+                theInfo += $"Control visible? {c.Visible} <br/>";
+                theInfo += $"ViewState? {c.EnableViewState} <br/>";
+            }
+        }
+        lblControlInfo.Text = theInfo;
+    }
+
+    protected void btnAddWidgets_Click(object sender, EventArgs e)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            var txtb = new TextBox { ID = $"newTextBox{i}" };
+            myPanel.Controls.Add(txtb);
+            ListControlsInPanel();
+        }
+    }
+
+    protected void btnClearPanel_Click(object sender, EventArgs e)
+    {
+        myPanel.Controls.Clear();
+        ListControlsInPanel();
+    }
+
+    protected void btnGetTxtData_Click(object sender, EventArgs e)
+    {
+        string lableData = $"<li>{Request.Form.Get("newTextBox0")}</li><br/>";
+        lableData += $"<li>{Request.Form.Get("newTextBox1")}</li><br/>";
+        lableData += $"<li>{Request.Form.Get("newTextBox2")}</li><br/>";
+        lblTextBoxData.Text = lableData;
+    }
+}
