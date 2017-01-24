008a9c3d8c5261d66a95d39ac32c13ee43be0a1c
aperepeliak <aperepelyak@gmail.com>
2017-01-24 19:48:48 +0200

    need to add known types for classes in the service

--- WCF/WCFHome/GenericService/GenericServiceHost/Program.cs
+++ WCF/WCFHome/GenericService/GenericServiceHost/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ServiceModel;
+using GenericServiceLib;
+
 namespace GenericServiceHost
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var host = new ServiceHost(typeof(GenericRepo)))
         }
     }
 }
