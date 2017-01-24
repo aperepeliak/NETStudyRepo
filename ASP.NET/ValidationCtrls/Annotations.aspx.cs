b96a9278e02dcac0261caa06e199491cd2eaea60
aperepeliak <aperepelyak@gmail.com>
2017-01-24 11:36:42 +0200

    add view form validation

---
+++ ASP.NET/ValidationCtrls/Annotations.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Annotations : System.Web.UI.Page
+{
+    private Inventory _model = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void SaveCar(Inventory car)
+    {
+        if (ModelState.IsValid)
+        {
+            _model = car;
+        }
+    }
+
+    public void UpdateCar(int carID)
+    {
+        Inventory car = new Inventory();
+        if(TryUpdateModel(car))
+        {
+            _model = car;
+        }
+    }
+
+    public Inventory GetCar() => _model;
+}
---
+++ ASP.NET/ValidationCtrls/App_Code/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+
+public class Inventory
+{
+    [Key,Required]
+    public int CarID { get; set; }
+
+    [Required(ErrorMessage ="Make is requried.")]
+    [StringLength(30, ErrorMessage ="Make can only be 30 characters or less.")]
+    public string Make { get; set; }
+
+    [Required, StringLength(30)]
+    public string Color { get; set; }
+
+    [StringLength(30, ErrorMessage = "PetName can only be 30 characters or less.")]
+    public string PetName { get; set; }
+}
