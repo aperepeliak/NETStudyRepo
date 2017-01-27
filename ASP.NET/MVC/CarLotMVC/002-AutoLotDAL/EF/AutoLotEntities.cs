0050c476829f07ef0028a401721dabeaa4860629
aperepeliak <aperepelyak@gmail.com>
2017-01-27 11:18:06 +0200

    move WebForms projects to separate folder and start fisrt MVC project

---
+++ ASP.NET/MVC/CarLotMVC/002-AutoLotDAL/EF/AutoLotEntities.cs
@@ -0,0 +1,20 @@
+namespace AutoLotDAL.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    using AutoLotDAL.Models;
+    using System.Web;
+
+    public class AutoLotEntities : DbContext
+    {
+        public AutoLotEntities()
+            : base("name=AutoLotConnection") { }
+
+        public virtual DbSet<CreditRisk> CreditRisks { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Inventory> Inventory { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
