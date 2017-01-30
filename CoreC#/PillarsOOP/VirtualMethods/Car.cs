a087fd82aceadf68b8f75464218b47a20613c1f9
aperepeliak <aperepelyak@gmail.com>
2017-01-30 15:28:58 +0200

    learning virtual methods aspects

---
+++ CoreC#/PillarsOOP/VirtualMethods/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    
+    class Car
+    {
+        public virtual void ShowDetails()
+        {
+            Console.WriteLine("A car object");
+        }
+    }
+
+    class ConvertibleCar : Car
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Convertible Car");
+        }
+    }
+
+    class ConvertibleCarDerive : ConvertibleCar
+    {
+        public virtual new void ShowDetails()
+        {
+            Console.WriteLine("ConvertibleDerive Car");
+        }
+    }
+
+    class ConvertibleCarDeriveDerive : ConvertibleCarDerive
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("ConvertibleDeriveDerive Car");
+        }
+    }
+
+    class Minivan : Car
+    {
+        public override void ShowDetails()
+        {
+            Console.WriteLine("Minivan Car");
+        }
+    }
+}
---
+++ CoreC#/PillarsOOP/VirtualMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car2 = new Minivan();
+            Car car1 = new ConvertibleCar();
+            Car car3 = new ConvertibleCarDerive();
+            Car car4 = new ConvertibleCarDeriveDerive();
+
+            car2.ShowDetails();
+            car1.ShowDetails();
+            car3.ShowDetails();
+            car4.ShowDetails();
+        }
+    }
+}
