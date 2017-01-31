c4efcf4f48edfff9281350ebf19f1e5da84c29f7
aperepeliak <aperepelyak@gmail.com>
2017-01-31 10:12:19 +0200

    learning MVC controllers

--- ASP.NET/MVC/CarLotMVC/CarLotMVC/Controllers/InventoryController.cs
+++ ASP.NET/MVC/CarLotMVC/CarLotMVC/Controllers/InventoryController.cs
@@ -9,17 +9,21 @@ using System.Web;
 using System.Web.Mvc;
 using AutoLotDAL.EF;
 using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using System.Data.Entity.Infrastructure;
 
 namespace CarLotMVC.Controllers
 {
     public class InventoryController : Controller
     {
-        private AutoLotEntities db = new AutoLotEntities();
+        //private AutoLotEntities db = new AutoLotEntities();
+
+        private readonly InventoryRepo _repo = new InventoryRepo();
 
         // GET: Inventory
         public async Task<ActionResult> Index()
         {
-            return View(await db.Inventory.ToListAsync());
+            return View(await _repo.GetAllAsync());
         }
 
         // GET: Inventory/Details/5
@@ -29,7 +33,7 @@ namespace CarLotMVC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = await db.Inventory.FindAsync(id);
+            var inventory = await _repo.GetOneAsync(id);
             if (inventory == null)
             {
                 return HttpNotFound();
@@ -48,16 +52,19 @@ namespace CarLotMVC.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "CarId,Make,Color,PetName")] Inventory inventory)
+        public async Task<ActionResult> Create([Bind(Include = "Make,Color,PetName")] Inventory inventory)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) { return View(inventory); }
+            try
             {
-                db.Inventory.Add(inventory);
-                await db.SaveChangesAsync();
+                await _repo.AddAsync(inventory);
                 return RedirectToAction("Index");
             }
-
-            return View(inventory);
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to create record: {ex.Message}");
+                return View(inventory);
+            }
         }
 
         // GET: Inventory/Edit/5
@@ -67,7 +74,7 @@ namespace CarLotMVC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = await db.Inventory.FindAsync(id);
+            Inventory inventory = await _repo.GetOneAsync(id);
             if (inventory == null)
             {
                 return HttpNotFound();
@@ -82,12 +89,22 @@ namespace CarLotMVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Edit([Bind(Include = "CarId,Make,Color,PetName")] Inventory inventory)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) { return View(inventory); }
+
+            try
             {
-                db.Entry(inventory).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                await _repo.SaveAsync(inventory);
                 return RedirectToAction("Index");
             }
+            catch (DbUpdateConcurrencyException)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save record. Another user updated the record.");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to save record: {ex.Message}");
+            }
+
             return View(inventory);
         }
 
@@ -98,7 +115,7 @@ namespace CarLotMVC.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Inventory inventory = await db.Inventory.FindAsync(id);
+            Inventory inventory = await _repo.GetOneAsync(id);
             if (inventory == null)
             {
                 return HttpNotFound();
@@ -107,21 +124,32 @@ namespace CarLotMVC.Controllers
         }
 
         // POST: Inventory/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public async Task<ActionResult> Delete([Bind(Include = "CarId")]Inventory inventory)
         {
-            Inventory inventory = await db.Inventory.FindAsync(id);
-            db.Inventory.Remove(inventory);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            try
+            {
+                await _repo.DeleteAsync(inventory);
+                return RedirectToAction("Index");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to delete record. Another user updated the record");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to delete record: {ex.Message}");
+            }
+
+            return View(inventory);
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
-                db.Dispose();
+                _repo.Dispose();
             }
             base.Dispose(disposing);
         }
