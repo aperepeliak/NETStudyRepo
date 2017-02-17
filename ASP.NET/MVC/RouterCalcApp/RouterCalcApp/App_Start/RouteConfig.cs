f8ef6e2c699ece4779d2e73a31f9e36f57bfb4c4
aperepeliak <aperepelyak@gmail.com>
2017-02-17 08:58:05 +0200

    add routerCalcApp

---
+++ ASP.NET/MVC/RouterCalcApp/RouterCalcApp/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RouterCalcApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapPageRoute("WebForm", "Calc", "~/WebForms/Calc.aspx");
+
+            routes.MapRoute(
+                name: "Calc",
+                url: "{controller}/{action}/{x}/{y}",
+                defaults: new { controller = "Calc", action = "Add", x = 0, y = 0 }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASP.NET/MVC/RouterCalcApp/RouterCalcApp/Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RouterCalcApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Add(int x, int y)
+        {
+            ViewBag.X = x;
+            ViewBag.Y = y;
+
+            return View();
+        }
+
+        public ActionResult Sub(int x, int y)
+        {
+            ViewBag.X = x;
+            ViewBag.Y = y;
+
+            return View();
+        }
+
+        public ActionResult Mul(int x, int y)
+        {
+            ViewBag.X = x;
+            ViewBag.Y = y;
+
+            return View();
+        }
+
+        public ActionResult Div(int x, int y)
+        {
+            ViewBag.X = x;
+            ViewBag.Y = y;
+
+            return View();
+        }
+    }
+}
