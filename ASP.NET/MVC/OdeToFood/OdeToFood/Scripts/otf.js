78886d4e4ca5cabb2c8d3da63f93e591bcf65dbc
aperepeliak <aperepelyak@gmail.com>
2017-02-24 17:05:00 +0200

    add custom script

---
+++ ASP.NET/MVC/OdeToFood/OdeToFood/Scripts/otf.js
@@ -0,0 +1,22 @@
+﻿$(() => {
+
+    var ajaxFormSubmit = function() {
+        var $form = $(this);
+
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done((data) => {
+            $($form.attr("data-otf-target"))
+            .replaceWith(data);
+        });
+
+        return false;
+    };
+
+    $("form[data-otf-ajax='true']").submit(ajaxFormSubmit);
+
+});
