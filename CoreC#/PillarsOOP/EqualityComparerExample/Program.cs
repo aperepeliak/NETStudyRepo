323d19bcd7a5d333c86e7e74ffd56ac45729350d
aperepeliak <aperepelyak@gmail.com>
2017-02-28 20:46:24 +0200

    add equality comparer example

---
+++ CoreC#/PillarsOOP/EqualityComparerExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityComparerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Customer("Dow", "Joe");
+            var c2 = new Customer("Dow", "Joe");
+
+            var d = new Dictionary<Customer, string>(new LastFirstEqComparer());
+            d[c1] = "Joe";
+            Console.WriteLine(d.ContainsKey(c2));
+        }
+    }
+}
