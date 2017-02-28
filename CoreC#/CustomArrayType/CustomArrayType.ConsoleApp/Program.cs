0609dfa3a820e4faadf99469bdf1e590dffca484
aperepeliak <aperepelyak@gmail.com>
2017-02-28 15:34:40 +0200

    add custom array type

---
+++ CoreC#/CustomArrayType/CustomArrayType.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using CustomArrayType.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArrayType.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomArray<string> test = new CustomArray<string>(5, -1);
+
+            test[1] = "hello";
+            test[2] = "world";
+            test[3] = null;
+
+            foreach (var item in test)
+            {
+                Console.WriteLine(item ?? "null");
+            }
+        }
+    }
+}
---
+++ CoreC#/CustomArrayType/CustomArrayType.Lib/CustomArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArrayType.Lib
+{
+    public class CustomArray<T> : IEnumerable
+    {
+        Array _arr;
+        public int Length => _arr.Length;
+        public int LowerBound => _arr.GetLowerBound(0);
+
+        public CustomArray(int length, int lowerBound)
+        {
+            _arr = Array
+                .CreateInstance(typeof(T), new int[] { length }, new int[] { lowerBound });
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < LowerBound || index >= (LowerBound + Length))
+                {
+                    throw new IndexOutOfRangeException
+                        ($"\nInvalid index.\nThe array is of length {Length} and lowerBound[{LowerBound}]");
+                }
+                return (T)_arr.GetValue(index);
+            }
+            set
+            {
+                if (index < LowerBound || index >= (LowerBound + Length))
+                {
+                    throw new IndexOutOfRangeException
+                        ($"\nInvalid index.\nThe array is of length {Length} and lowerBound[{LowerBound}]");
+                }
+                _arr.SetValue(value, index);
+            }
+        }
+
+        public IEnumerator GetEnumerator() => _arr.GetEnumerator();
+    }
+}
