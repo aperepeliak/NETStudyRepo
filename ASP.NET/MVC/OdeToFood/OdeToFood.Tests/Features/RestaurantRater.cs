aad82ca057357c9b63229791f0577db7128f2399
aperepeliak <aperepelyak@gmail.com>
2017-03-02 21:20:59 +0200

    learning tdd basics

---
+++ ASP.NET/MVC/OdeToFood/OdeToFood.Tests/Features/RestaurantRater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using OdeToFood.Models;
+using System.Linq;
+
+namespace OdeToFood.Tests.Features
+{
+    internal class RestaurantRater
+    {
+        private Restaurant _restaurant;
+
+        public RestaurantRater(Restaurant restaurant)
+        {
+            _restaurant = restaurant;
+        }
+
+        public RatingResult ComputeRating(int numberOfReviews)
+        {
+            var result = new RatingResult();
+            result.Rating = (int)_restaurant.Reviews.Average(r => r.Rating);
+
+            return result;
+        }
+    }
+}
