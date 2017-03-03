eb22f14ec58783986ec4c9d1cadff66fecf25b08
aperepeliak <aperepelyak@gmail.com>
2017-03-03 16:50:44 +0200

    learning tdd basics

---
+++ Testing/SalaryCalcTest/Calculator/SalaryCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalaryCalcTest
+{
+    public class SalaryCalculator
+    {
+        const int HOURS_IN_YEAR = 2080;
+
+        public SalaryCalculator() { }
+
+        public decimal GetAnnualSalaryMethod(decimal hourlyWage) => HOURS_IN_YEAR * hourlyWage;
+        public decimal GetHourlyWage(int annualSalary) => annualSalary / HOURS_IN_YEAR;
+    }
+}
---
+++ Testing/SalaryCalcTest/SalaryCalcTest/CalcTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalaryCalcTest;
+
+namespace SalaryCalcTest
+{
+    [TestClass]
+    public class CalcTest
+    {
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalaryMethod(50);
+
+            // Assert
+            Assert.AreEqual(104000, annualSalary);
+        }
+
+        [TestMethod]
+        public void HourlyWageTest()
+        {
+            SalaryCalculator sc = new SalaryCalculator();
+
+            decimal hourlyWage = sc.GetHourlyWage(52000);
+
+            Assert.AreEqual(25, hourlyWage);
+        }
+    }
+}
