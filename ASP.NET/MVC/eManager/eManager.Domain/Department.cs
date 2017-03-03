811646b775a32ed68b29105157c6cf93746be02c
aperepeliak <aperepelyak@gmail.com>
2017-03-03 19:32:04 +0200

    init new project, learning IoC

---
+++ ASP.NET/MVC/eManager/eManager.Domain/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace eManager.Domain
+{
+    public class Department
+    {
+        public int      Id { get; set; }
+        public string   Name { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
