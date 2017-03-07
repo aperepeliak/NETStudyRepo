aa0ee227e3a7e63c9de934a41ad30266cdf7e0c5
aperepeliak <aperepelyak@gmail.com>
2017-03-07 20:32:22 +0200

    learning instantiating generic types through reflection

---
+++ CoreC#/SA-GenericReflection/SA-GenericReflection/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace SA_GenericReflection
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public void Speak<T>() => System.Console.WriteLine(typeof(T).Name);
+    }
+}
---
+++ CoreC#/SA-GenericReflection/SA-GenericReflection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_GenericReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));
+            Console.Write(employeeList.GetType().Name);
+
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+            foreach (var arg in genericArguments)
+            {
+                Console.Write($"[{arg.Name}]");
+            }
+            Console.WriteLine();
+
+            var employee = new Employee();
+            var employeeType = typeof(Employee);
+            var methodInfo = employeeType.GetMethod("Speak");
+            methodInfo = methodInfo.MakeGenericMethod(typeof(DateTime));
+
+            methodInfo.Invoke(employee, null);
+        }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+}
