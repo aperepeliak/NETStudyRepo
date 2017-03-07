4fcf85003ea9924c69e7c566c5bcf16decfd31ff
aperepeliak <aperepelyak@gmail.com>
2017-03-07 21:33:23 +0200

    implementing custom ioc container

---
+++ CoreC#/SA-GenericReflection/SA-GenericReflection/Services.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_GenericReflection
+{
+    public interface ILogger
+    {
+        
+    }
+
+    public class SqlServerLogger : ILogger
+    {
+
+    }
+
+    public interface IRepo<T>
+    {
+
+    }
+
+    public class SqlRepo<T> : IRepo<T>
+    {
+        public SqlRepo(ILogger logger)
+        {
+
+        }
+    }
+
+    public class Customer
+    {
+
+    }
+
+    public class InvoiceService
+    {
+        public InvoiceService(IRepo<Customer> repo, ILogger logger)
+        {
+
+        }
+    }
+}
---
+++ CoreC#/SA-GenericReflection/UnitTests/IoCTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SA_GenericReflection;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class IoCTests
+    {
+        [TestMethod]
+        public void Can_Resolve_Types()
+        {
+            var ioc = new Container(); 
+            ioc.For<ILogger>().Use<SqlServerLogger>();
+
+            var logger = ioc.Resolve<ILogger>();
+
+            Assert.AreEqual(typeof(SqlServerLogger), logger.GetType());
+        }
+
+        [TestMethod]
+        public void Can_Resolve_Types_Without_Default_Ctor()
+        {
+            var ioc = new Container();
+            ioc.For<ILogger>().Use<SqlServerLogger>();
+            ioc.For<IRepo<Employee>>().Use<SqlRepo<Employee>>();
+
+            var repo = ioc.Resolve<IRepo<Employee>>();
+
+            Assert.AreEqual(typeof(SqlRepo<Employee>), repo.GetType());
+        }
+
+        [TestMethod]
+        public void Can_Resolve_Concrete_Type()
+        {
+            var ioc = new Container();
+            ioc.For<ILogger>().Use<SqlServerLogger>();
+            ioc.For(typeof(IRepo<>)).Use(typeof(SqlRepo<>));
+
+            var service = ioc.Resolve<InvoiceService>();
+
+            Assert.IsNotNull(service);
+        }
+    }
+}
