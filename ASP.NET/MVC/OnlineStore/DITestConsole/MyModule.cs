1628a1c639c7cc869b045272e4056231fc3194c9
aperepeliak <aperepelyak@gmail.com>
2017-03-16 20:51:59 +0200

    learning DI

---
+++ ASP.NET/MVC/OnlineStore/DITestConsole/MyModule.cs
@@ -0,0 +1,20 @@
+﻿using Ninject.Modules;
+using OnlineStore.DAL.Entities;
+using OnlineStore.Repo.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITestConsole
+{
+    public class MyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            //Bind<DbContext>().To<ShopContext>().InRequestScope();
+            Bind(typeof(IGenericRepo<>)).To(typeof(GenericRepo<>)).InSingletonScope();
+        }
+    }
+}
---
+++ ASP.NET/MVC/OnlineStore/DITestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Ninject;
+using OnlineStore.DAL.Entities;
+using OnlineStore.Repo.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DITestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<DbContext>().To<ShopContext>();
+            kernel.Bind(typeof(IGenericRepo<>)).To(typeof(GenericRepo<>));
+
+            IGenericRepo<Good> goods = kernel.Get<IGenericRepo<Good>>();
+
+            foreach (var item in goods.GetAll())
+            {
+                Console.WriteLine($"{item.GoodName}");
+            }
+        }
+    }
+}
