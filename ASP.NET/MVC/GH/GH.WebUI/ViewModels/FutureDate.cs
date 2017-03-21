02d47080cf4e59a142876afc88a5731e5b730361
aperepeliak <aperepelyak@gmail.com>
2017-03-21 17:19:00 +0200

    add client-side validation

---
+++ ASP.NET/MVC/GH/GH.WebUI/ViewModels/FutureDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GH.WebUI.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+            bool isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "d MMM yyyy",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out dateTime);
+
+            return (isValid && dateTime > DateTime.Now);
+        }
+    }
+}
--- ASP.NET/MVC/GH/GH.WebUI/ViewModels/GigFormViewModel.cs
+++ ASP.NET/MVC/GH/GH.WebUI/ViewModels/GigFormViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using GH.WebUI.Models;
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GH.WebUI.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
+        public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
     }
 }
