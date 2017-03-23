6c45495e6a5e2a4f1db5600eaf516deff6bd250e
aperepeliak <aperepelyak@gmail.com>
2017-03-23 19:58:19 +0200

    implement update action

--- ASP.NET/MVC/GH/GH.WebUI/Controllers/GigsController.cs
+++ ASP.NET/MVC/GH/GH.WebUI/Controllers/GigsController.cs
@@ -57,10 +57,54 @@ namespace GH.WebUI.Controllers
         {
             var viewModel = new GigFormViewModel
             {
-                Genres = _context.Genres.ToList()
+                Genres = _context.Genres.ToList(),
+                Heading = "Add a Gig"
             };
 
-            return View(viewModel);
+            return View("GigForm", viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+
+            var viewModel = new GigFormViewModel
+            {
+                Id = gig.Id,
+                Genres = _context.Genres.ToList(),
+                Date = gig.DateTime.ToString("d MMM yyyy"),
+                Time = gig.DateTime.ToString("HH:mm"),
+                Genre = gig.GenreId,
+                Venue = gig.Venue,
+                Heading = "Edit a Gig"
+            };
+
+            return View("GigForm", viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(GigFormViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("GigForm", viewModel);
+            }
+
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == viewModel.Id && g.ArtistId == userId);
+
+            gig.Venue = viewModel.Venue;
+            gig.DateTime = viewModel.GetDateTime();
+            gig.GenreId = viewModel.Genre;
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Mine", "Gigs");
         }
 
         [Authorize]
@@ -71,7 +115,7 @@ namespace GH.WebUI.Controllers
             if (!ModelState.IsValid)
             {
                 viewModel.Genres = _context.Genres.ToList();
-                return View("Create", viewModel);
+                return View("GigForm", viewModel);
             }
 
             var gig = new Gig
--- ASP.NET/MVC/GH/GH.WebUI/ViewModels/GigFormViewModel.cs
+++ ASP.NET/MVC/GH/GH.WebUI/ViewModels/GigFormViewModel.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using GH.WebUI.Controllers;
+using System.Web.Mvc;
 
 namespace GH.WebUI.ViewModels
 {
     public class GigFormViewModel
     {
+        public int Id { get; set; }
+
         [Required]
         public string Venue { get; set; }
 
@@ -21,6 +26,22 @@ namespace GH.WebUI.ViewModels
         [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+
+        public string Heading { get; set; }
+        public string Action
+        {
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update =
+                    ((c) => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create =
+                    ((c) => c.Create(this));
+
+                var action = (Id != 0) ? update : create;
+                return (action.Body as MethodCallExpression).Method.Name; 
+            }
+        }
 
         public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
     }
