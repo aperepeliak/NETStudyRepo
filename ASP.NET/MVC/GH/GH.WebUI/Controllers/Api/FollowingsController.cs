17155159c60ebd9260ea4b06d95959b2c5ceaa31
aperepeliak <aperepelyak@gmail.com>
2017-03-23 21:13:30 +0200

    add bootbox, move apis to folder

--- ASP.NET/MVC/GH/GH.WebUI/Controllers/Api/FollowingsController.cs
+++ ASP.NET/MVC/GH/GH.WebUI/Controllers/Api/FollowingsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNet.Identity;
 using System.Linq;
 using System.Web.Http;
 
-namespace GH.WebUI.Controllers
+namespace GH.WebUI.Controllers.Api
 {
     public class FollowingsController : ApiController
     {
---
+++ ASP.NET/MVC/GH/GH.WebUI/Controllers/Api/GigsController.cs
@@ -0,0 +1,35 @@
+﻿using GH.WebUI.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GH.WebUI.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        ApplicationDbContext _context;
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var gig = _context.Gigs.Single(g => g.Id == id && userId == g.ArtistId);
+
+            if (gig.IsCanceled) return NotFound();
+
+            gig.IsCanceled = true;
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
