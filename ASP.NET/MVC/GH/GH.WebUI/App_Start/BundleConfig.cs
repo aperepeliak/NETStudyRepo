afde32f9c749d13e5fe784ab1dc1ca8826a5bcbc
aperepeliak <aperepelyak@gmail.com>
2017-03-27 15:28:44 +0300

    refactoring js code

--- ASP.NET/MVC/GH/GH.WebUI/App_Start/BundleConfig.cs
+++ ASP.NET/MVC/GH/GH.WebUI/App_Start/BundleConfig.cs
@@ -8,6 +8,10 @@ namespace GH.WebUI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/scripts/app/app.js"
+                ));
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
