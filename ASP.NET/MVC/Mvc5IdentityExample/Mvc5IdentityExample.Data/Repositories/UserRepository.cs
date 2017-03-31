1249c91f2ce732a3deb9700662038a1ced22b53a
aperepeliak <aperepelyak@gmail.com>
2017-03-31 21:55:11 +0300

    working on identity in data layer

---
+++ ASP.NET/MVC/Mvc5IdentityExample/Mvc5IdentityExample.Data/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using Mvc5IdentityExample.Domain.Entities;
+using Mvc5IdentityExample.Domain.Repositories;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Mvc5IdentityExample.Data.Repositories
+{
+    internal class UserRepository : Repository<User>, IUserRepository
+    {
+        internal UserRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public User FindByEmail(string email)
+        {
+            return Set.FirstOrDefault(x => x.UserName == email);
+        }
+
+        public Task<User> FindByEmailAsync(string email)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == email);
+        }
+
+        public Task<User> FindByEmailAsync(CancellationToken cancellationToken, string email)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == email, cancellationToken);
+        }
+
+        public User FindByUserName(string username)
+        {
+            return Set.FirstOrDefault(x => x.UserName == username);
+        }
+
+        public Task<User> FindByUserNameAsync(string username)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public Task<User> FindByUserNameAsync(CancellationToken cancellationToken, string username)
+        {
+            return Set.FirstOrDefaultAsync(x => x.UserName == username, cancellationToken);
+        }
+    }
+}
