6f30a253a3ba3231c692adb58344af66ab27d2dc
aperepeliak <aperepelyak@gmail.com>
2017-03-31 20:03:56 +0300

    stuck on userManager

---
+++ ASP.NET/MVC/IdentityExperiments/BAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,13 @@
+﻿using BAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store)
+                    : base(store)
+        { }
+    }
+}
---
+++ ASP.NET/MVC/IdentityExperiments/BAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,13 @@
+﻿using BAL.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace BAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+                : base(store)
+        {
+        }
+    }
+}
