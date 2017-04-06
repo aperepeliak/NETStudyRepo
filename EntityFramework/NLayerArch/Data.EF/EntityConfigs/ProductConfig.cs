26abfab6851747f6f44a2087c0f77c17f4c1182b
aperepeliak <aperepelyak@gmail.com>
2017-04-06 12:08:46 +0300

    enable migrations, create db

---
+++ EntityFramework/NLayerArch/Data.EF/EntityConfigs/ProductConfig.cs
@@ -0,0 +1,27 @@
+﻿using DomainLayer.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Data.EF.EntityConfigs
+{
+    public class ProductConfig : EntityTypeConfiguration<Product>
+    {
+        public ProductConfig()
+        {
+            Property(p => p.CategoryId)
+                .IsRequired();
+
+            Property(p => p.SupplierId)
+                .IsRequired();
+
+            Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            HasRequired(p => p.Category)
+                .WithMany(c => c.Products);
+
+            HasRequired(p => p.Supplier)
+                .WithMany(s => s.Products);
+        }
+    }
+}
---
+++ EntityFramework/NLayerArch/Data.EF/EntityConfigs/SupplierConfig.cs
@@ -0,0 +1,15 @@
+﻿using DomainLayer.Models;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Data.EF.EntityConfigs
+{
+    public class SupplierConfig : EntityTypeConfiguration<Supplier>
+    {
+        public SupplierConfig()
+        {
+            Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
