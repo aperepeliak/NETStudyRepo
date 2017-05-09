1d94c6152f53523cd22ffea9c56fe722958dba46
aperepeliak <aperepelyak@gmail.com>
2017-05-09 10:08:00 +0300

    add custom stack

---
+++ CoreC#/MH-CSharp/002-Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Stack
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Clear();
+            stack.Push(4);
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ CoreC#/MH-CSharp/002-Stack/Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _002_Stack
+{
+    public class Stack
+    {
+        private List<object> _container;
+        private int _head;
+
+        public Stack()
+        {
+            _container = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Storing nulls is forbidden");
+
+            _container.Add(obj);
+            _head++;
+        }
+
+        public object Pop()
+        {
+            if (_head == 0)
+                throw new InvalidOperationException("The stack is empty.");
+
+            _head--;
+            var obj = _container[_head];
+
+            _container = _container.Take(_head).ToList();
+            return obj;
+        }
+
+        public void Clear()
+        {
+            _container = new List<object>();
+            _head = 0;
+        }
+    }
+}
